39ebc19a9900b23304f8d2130c57403b4a08f7d0
christopher.hesse <christopher.hesse@06a9fef0-fcd4-11dd-bc25-5f29055a2a2d>
2009-02-22 12:29:56 +0000

    fuck you svn

--- trunk/bot.py
+++ trunk/bot.py
@@ -1,7 +1,10 @@
 class Bot(object):
-    def __init__(self, id, credits, *args, **kwargs):
+    def __init__(self, id, initial_credits, big_blind_amount, small_blind_amount, *args, **kwargs):
         self.id = id
-        self.credits = credits
+        self.initial_credits = initial_credits
+        self.credits = self.initial_credits
+        self.big_blind_amount = big_blind_amount
+        self.small_blind_amount = small_blind_amount
         self.event_queue = []
     
     def turn(self):
