1b2af461975d82ad5f6d82c29fd481bf932543ca
christopher.hesse <christopher.hesse@06a9fef0-fcd4-11dd-bc25-5f29055a2a2d>
2009-02-23 00:15:57 +0000

    pure python bots

--- trunk/main.py
+++ trunk/main.py
@@ -4,9 +4,9 @@ from poker_game import PokerGame
 
 def main():
    from example_bots.example_bot import ExampleBot
-   from example_bots.protobot.cpp.foldbot_cpp import FoldBot
+   #from example_bots.protobot.cpp.foldbot_cpp import FoldBot
    #from example_bots.protobot.java.foldbot_java import FoldBot
-   bots = [ExampleBot, FoldBot]
+   bots = [ExampleBot, ExampleBot]
    game = PokerGame(bots=bots)
    start_time = time.time()
    outcome = game.run()
