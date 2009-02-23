ff07689982525fbac9b9c1342b20935f0624045d
christopher.hesse <christopher.hesse@06a9fef0-fcd4-11dd-bc25-5f29055a2a2d>
2009-02-23 00:10:30 +0000

    pause in windows

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import time
-
+import platform
 from poker_game import PokerGame
 
 def main():
@@ -13,6 +13,8 @@ def main():
    end_time = time.time()
    print "Result:", outcome
    print "Time elapsed:", (end_time - start_time), "seconds"
+   if platform.system() = 'Windows':
+       raw_input('Press enter to continue')
 
 if __name__ == "__main__":
     import sys
