653ffb86266dacdce309d13c40c1cd12d096fe07
christopher.hesse <christopher.hesse@06a9fef0-fcd4-11dd-bc25-5f29055a2a2d>
2009-02-23 00:31:37 +0000

    windows :(

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ from poker_game import PokerGame
 
 def main():
    from example_bots.example_bot import ExampleBot
-   #from example_bots.protobot.cpp.foldbot_cpp import FoldBot
-   #from example_bots.protobot.java.foldbot_java import FoldBot
+   from example_bots.protobot.cpp.foldbot_cpp import FoldBot as FoldBotCpp
+   from example_bots.protobot.java.foldbot_java import FoldBot as FoldBotJava
+   # uncomment this line to run the CPP foldbot
+   #bots = [ExampleBot, FoldBotCpp]
    bots = [ExampleBot, ExampleBot]
    game = PokerGame(bots=bots)
    start_time = time.time()
@@ -21,6 +23,7 @@ if __name__ == "__main__":
     try:
         sys.exit(main())
     except Exception, e:
+        print ""
         traceback.print_exc()
         if platform.system() == 'Windows':
             raw_input('\nPress enter to continue')
